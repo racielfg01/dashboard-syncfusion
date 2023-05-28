9c1f0f22223935237c2e4c8777d170f88e745ce9
Raciel FG <racielfg01@gmail.com>
2023-05-27 20:10:54 -0400

    actualizacion del fondo de la grafica stacked

--- src/pages/Charts/Stacked.jsx
+++ src/pages/Charts/Stacked.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
 import { ChartsHeader, Stacked as StackedChart } from '../../components';
+import { useStateContext } from '../../contexts/ContextProvider';
 
-const Stacked = () => (
-  <div className="m-4 md:m-10 mt-24 p-10 bg-gray-600 dark:bg-secondary-dark-bg rounded-3xl">
+const Stacked = () => {
+  const { currentMode } = useStateContext();
+  return (
+  <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
     <ChartsHeader category="Stacked" title="Revenue Breakdown" />
     <div className="w-full">
-      <StackedChart />
+      <StackedChart  currentMode={currentMode}/>
     </div>
   </div>
 );
-
+}
 export default Stacked;
